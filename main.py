ece779b4d5a5627b5cc143d77e6060b43128d655
Henry <henrynyoike888@gmail.com>
2026-01-21 12:37:24 +0300

    Reduced the number of Conv2d and Pools but loss functions' values are still same

--- main.py
+++ main.py
@@ -31,33 +31,53 @@ class HomerBartDataset(Dataset):
         return self.data.classes
     
 class HomerBartClassifier(nn.Module):
+    # def __init__(self, num_classes : int = 2):
+    #     super(HomerBartClassifier , self).__init__()
+        
+    #     #Conv2d calculation ->out_size = (input_size - Kernel)/stride + 1 -> Output shape -> (out_channels , out_size , out_size)
+    #     self.conv1 = nn.Conv2d(3 , 12 , 5) # (12,28,28)
+    #     self.pool = nn.MaxPool2d(2,2) #(12,14,14)
+    #     self.conv2 = nn.Conv2d(12 , 64 , 5) # (64 , 28 , 28) -> Pool -> (64 , 14 , 14)
+    #     #(24 , 10 , 10) -> Pool -> (24,5,5) -> Flatten -> (24*5*5)
+    #     self.conv3 = nn.Conv2d(64 , 32 , 5) # (32 , 28 , 28) -> Pool ->  (32 , 14 , 14)  -> Flatten -> (32*14*14)
+    #     self.relu = nn.ReLU()
+    #     self.fc1 = nn.Linear(32*14*14 , 84)
+    #     self.fc2 = nn.Linear(84 , 12)
+    #     self.fc3 = nn.Linear(12 , num_classes)
+        
     def __init__(self, num_classes : int = 2):
         super(HomerBartClassifier , self).__init__()
         
-        #Conv2d calculation ->out_size = (input_size - Kernel)/stride + 1 -> Output shape -> (out_channels , out_size , out_size)
-        self.conv1 = nn.Conv2d(3 , 12 , 5) # (12,28,28)
-        self.pool = nn.MaxPool2d(2,2) #(12,14,14)
-        self.conv2 = nn.Conv2d(12 , 64 , 5) # (64 , 28 , 28) -> Pool -> (64 , 14 , 14)
-        #(24 , 10 , 10) -> Pool -> (24,5,5) -> Flatten -> (24*5*5)
-        self.conv3 = nn.Conv2d(64 , 32 , 5) # (32 , 28 , 28) -> Pool ->  (32 , 14 , 14)  -> Flatten -> (32*14*14)
-        self.relu = nn.ReLU()
-        self.fc1 = nn.Linear(32*14*14 , 84)
-        self.fc2 = nn.Linear(84 , 12)
-        self.fc3 = nn.Linear(12 , num_classes)
+        self.conv1 = nn.Conv2d(in_channels=3 , out_channels=12 , kernel_size=5) #(12,  28 , 28)
+        self.pool = nn.MaxPool2d(2,2) #12,14,14 -> flatten -> 12*14*14
+        self.fc1 = nn.Linear(12*14*14 , 120)
+        self.fc2 = nn.Linear(120 , 84)
+        self.fc3 = nn.Linear(84 , num_classes)
         
-    def forward(self , X):
-        out = self.relu((self.conv1(X)))
-        out = self.pool(out)
-        out = self.relu(self.conv2(out))
-        out = self.pool(out)
-        # out = self.relu(self.conv3(out))
-        out = self.pool(out)
-        out = torch.flatten(out , 1 )
-        out = self.relu(self.fc1(out))
-        out = self.relu(self.fc2(out))
+    
+    def forward(self , X): 
+        out = self.conv1(X)
+        out = torch.relu(self.pool(out))
+        out = torch.flatten(out , 1)
+        out = self.fc1(out)
+        out = torch.relu(self.fc2(out))
         out = self.fc3(out)
         
-        return out            
+        return out    
+    
+    # def forward(self , X):
+    #     out = self.relu((self.conv1(X)))
+    #     out = self.pool(out)
+    #     out = self.relu(self.conv2(out))
+    #     out = self.pool(out)
+    #     # out = self.relu(self.conv3(out))
+    #     out = self.pool(out)
+    #     out = torch.flatten(out , 1 )
+    #     out = self.relu(self.fc1(out))
+    #     out = self.relu(self.fc2(out))
+    #     out = self.fc3(out)
+        
+    #     return out            
 
 #Get the class and index of the category folders
 index_class = {idx : category for category , idx in ImageFolder(homer_bart_dir).class_to_idx.items()}
@@ -78,10 +98,11 @@ hb_model = HomerBartClassifier()
 epochs , learning_rate = 100 , 0.001
 
 loss_fn = nn.CrossEntropyLoss()
-optimizer = optim.Adam(params = hb_model.parameters() , lr = learning_rate)
+optimizer = optim.Adam(params = hb_model.parameters() , lr = learning_rate , maximize=True , weight_decay=5.0)
 
 for epoch in range(epochs):
     for image , label in homer_bart_dataloader:
+        optimizer.zero_grad()
         pred_label = hb_model(image)
         
         # pred_label = torch.tensor(pred_label , dtype=torch.float , requires_grad=True)
@@ -90,8 +111,6 @@ for epoch in range(epochs):
         loss = loss_fn(pred_label , label)
         
         loss.backward()
-        
-        optimizer.zero_grad()
         
         optimizer.step()
 
